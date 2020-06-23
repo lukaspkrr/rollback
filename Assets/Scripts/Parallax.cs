37ce7d716d0f88a107fd4d965176d534ebd27b37
p21lucasm <lucas.marques@p21sistemas.com.br>
2020-06-22 23:00:47 -0300

    Add classe de parallax

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour {
+    private float length, startPosition;
+    public GameObject camera;
+    public float parallaxEffect;
+
+    // Start is called before the first frame update
+    void Start() {
+        startPosition = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate() {
+        float temp = (camera.transform.position.x * (1 - parallaxEffect));
+        float distance = (camera.transform.position.x * parallaxEffect);
+
+        transform.position = new Vector3(startPosition + distance, transform.position.y, transform.position.z);
+
+        if (temp > startPosition + length) startPosition += length;
+        else if (temp < startPosition - length) startPosition -= length;
+    }
+}
