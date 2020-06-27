144c483d34fb715f588844c2868e5f2167ec1d80
Marcelo Santos <marcelo_mr.s@hotmail.com>
2020-06-27 19:44:14 -0300

    Iniciando movimentação do Player

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Player : MonoBehaviour {
+    // Start is called before the first frame update
+    public SpriteRenderer rendererRef;
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        Move();
+        
+    }
+
+    void Move() {
+        Vector3 pos = transform.position;
+        if(Input.GetKey(KeyCode.RightArrow)){
+            pos.x += 0.1f;
+            rendererRef.flipX = false;
+        } else if (Input.GetKey(KeyCode.LeftArrow)){
+            pos.x += -0.1f;
+            rendererRef.flipX = true;
+        }
+        transform.position = pos;
+    }
+}
