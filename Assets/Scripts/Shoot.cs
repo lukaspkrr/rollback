0cc14d103a4c45eb5812906b2454e4543dc14c71
Marcelo Santos <marcelo_mr.s@hotmail.com>
2020-06-29 22:22:12 -0300

    inciando tiro com arco. erro de timing

---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    public float shotSpeed = 15;
+    // Start is called before the first frame update
+   
+
+    public void Shooting() {
+    //    int shotDirection = 1;
+
+    //     if(directionFoward){
+    //         shotDirection = 1;
+    //     }else{
+    //        shotDirection = -1; 
+    //     }
+    //     newShot.velocity = Vector2.right * shotSpeed * shotDirection;
+    Debug.Log("atirou flecha");
+    }
+}
