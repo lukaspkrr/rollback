4630486c9bd893fba81779965e63c62326a8b917
munhozfontana <munhozfontana@gmail.com>
2020-07-05 00:41:00 -0300

    Add menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void NewGame()
+    {
+        SceneManager.LoadScene("Fase 1", LoadSceneMode.Single);
+        
+    }
+
+    // Update is called once per frame
+    
+    public void Exit() {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Aplication.Quit();
+        #endif
+    }
+}
