7bd82b1359baa7f207a8df279109431406a93251
Marcelo Santos <marcelo_mr.s@hotmail.com>
2020-07-05 16:15:37 -0300

    setando fim de jogo

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -1,19 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EnemyHealth : Damageable
 {
     private Animator anim;
+    private Animator animWin;
     private float Life;
 
     private float OriginalScale;
 
     private GameObject BossLifeBar;
+    private GameObject TextVictory;
     // Start is called before the first frame update
     public override void Death(){
         anim.SetBool("die", true);
-        Invoke("DestroyAferDeath", 1f);
+        Invoke("DestroyAferDeath", 3f);
     }
     public override void ReduceLifeBar(int currentHealth, int maxHealth){
 
@@ -21,11 +24,15 @@ public class EnemyHealth : Damageable
             Life = OriginalScale * (float)currentHealth /maxHealth;
             float LifeY = BossLifeBar.gameObject.transform.localScale.y;
             float LifeZ = BossLifeBar.gameObject.transform.localScale.z;
+            Debug.Log(TextVictory);
             if(currentHealth > 0){
                 
                 BossLifeBar.gameObject.transform.localScale = new Vector3(Life ,LifeY,LifeZ);
             } else{
                 BossLifeBar.gameObject.transform.localScale = new Vector3(0,LifeY,LifeZ);
+                Invoke("ReturnToMenu", 2.5f );
+                animWin.SetBool("win", true);
+                
             }
         }
         
@@ -42,17 +49,25 @@ public class EnemyHealth : Damageable
     void Start()
     {
         anim =  GetComponent<Animator>();
+        
         base.Start();
         try {
         BossLifeBar = GameObject.FindWithTag("BossLifeBar");
+        TextVictory = GameObject.FindWithTag("TextVictory");
             if(BossLifeBar){
                 OriginalScale = BossLifeBar.gameObject.transform.localScale.x;
+                animWin =  TextVictory.GetComponent<Animator>();
             }
         }
         catch (System.Exception) {
+            
             Debug.Log("SemBarra");
         }
         
+    }
+
+    void ReturnToMenu() {
+        SceneManager.LoadScene(0);
     }
 
 }
