0688dd264ffd7b27b6467a2ae3a098e4f0f88a92
Marcelo Santos <marcelo_mr.s@hotmail.com>
2020-07-06 22:10:15 -0300

    FireBreath

---
+++ Assets/Scripts/BossBreath.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossBreath : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float breathingTime;
+    public float breathReloadTime;
+    private float breathTimer = 0f;
+
+    public Animator anim;
+    void Start()
+    {
+        
+        anim =  GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update(){
+        Breath();
+    }
+
+    void Breath(){
+        breathTimer += Time.deltaTime;
+        if(breathTimer >= breathReloadTime){
+            anim.SetBool("attack", true);
+            Invoke("FinishBreath",breathingTime);
+        }
+    }
+
+
+    void FinishBreath(){
+        anim.SetBool("attack", false);
+        breathTimer = 0f;
+        breathTimer += Time.deltaTime;
+    }
+}
